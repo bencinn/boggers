b1490077484b4c4e247adf07d59b9242cbe4d965
hansnnn <pp.rongpan@gmail.com>
2023-04-19 00:01:57 +0700

    Adding sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://blog-hansnnn.vercel.app/",
+  generateRobotsTxt: true,
+};
--- next.config.js
+++ next.config.js
@@ -6,13 +6,13 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '/u/*',
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/*",
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
