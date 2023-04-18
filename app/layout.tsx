25a473277febcf9bb9d8f56193641410fdc984dd
hansnnn <pp.rongpan@gmail.com>
2023-04-18 23:42:45 +0700

    Adding Google search

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content={process.env.GOOGLE_VERIFICATION} />
+      </head>
       <body>
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
           <div className="z-10 w-full max-w-5xl justify-between font-mono text-sm lg:flex flex-col">
